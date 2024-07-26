4124e3ed55a90ce8f82b9cee91ee8820fd46649b
robpSF <robert@tstoryteller.com>
2024-07-26 16:21:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -197,13 +197,13 @@ if uploaded_file is not None or 'persona_details_df' in st.session_state:
     buffer = BytesIO()
     with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
         persona_details_df.to_excel(writer, index=False, sheet_name='Sheet1')
-    buffer.seek(0)  
+    buffer.seek(0)
     st.download_button(
         label="Download Excel file",
         data=buffer,
         file_name="modified_persona_details.xlsx",
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        key="download_button"
+        key=get_unique_key()
     )
 
 else:
