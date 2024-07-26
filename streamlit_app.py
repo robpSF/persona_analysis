372d4a96bab49178a5e242f3452e94a3a4178004
robpSF <robert@tstoryteller.com>
2024-07-26 14:15:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 
 # Streamlit application title
 st.title("Persona Analysis Dashboard")
@@ -18,6 +19,9 @@ if uploaded_file is not None:
     tags = persona_details_df['Tags'].str.split(',', expand=True).stack()
     tags_with_factions = persona_details_df[['Faction', 'Tags']].copy()
     tags_with_factions = tags_with_factions.set_index('Faction')['Tags'].str.split(',', expand=True).stack().reset_index(name='Tag')
+
+    # Set font size for charts
+    plt.rcParams.update({'font.size': 8})
 
     # Chart (a): Horizontal bar chart of number of personas per Faction
     st.subheader("Number of Personas per Faction")
@@ -42,11 +46,28 @@ if uploaded_file is not None:
     # Chart (c): Number of Personas per Tags within each Faction
     st.subheader("Number of Personas per Tags within each Faction")
     tag_faction_counts = tags_with_factions.groupby(['Faction', 'Tag']).size().unstack(fill_value=0)
-    fig, ax = plt.subplots(figsize=(10, 8))
-    sns.heatmap(tag_faction_counts, annot=True, fmt="d", cmap="YlGnBu", ax=ax)
-    ax.set_xlabel("Tags")
-    ax.set_ylabel("Faction")
-    ax.set_title("Number of Personas per Tags within each Faction")
-    st.pyplot(fig)
+
+    # Use Plotly for the heatmap with hover functionality
+    fig = px.imshow(
+        tag_faction_counts,
+        labels=dict(x="Tags", y="Faction", color="Number of Personas"),
+        x=tag_faction_counts.columns,
+        y=tag_faction_counts.index,
+        color_continuous_scale="YlGnBu",
+        text_auto=False
+    )
+
+    fig.update_traces(
+        hovertemplate="<b>Faction: %{y}</b><br>Tag: %{x}<br>Number of Personas: %{z}<extra></extra>"
+    )
+
+    fig.update_layout(
+        title="Number of Personas per Tags within each Faction",
+        xaxis_title="Tags",
+        yaxis_title="Faction",
+        font=dict(size=8)
+    )
+
+    st.plotly_chart(fig)
 else:
     st.write("Please upload an Excel file to proceed.")
